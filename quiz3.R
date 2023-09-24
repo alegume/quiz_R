904f6572c5e7fb5fb64bb83d9e010ad0c5d7ac81
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2023-09-24 13:44:42 -0300

    finishing ex4

--- quiz3.R
+++ quiz3.R
@@ -1,4 +1,4 @@
-# Exercício 3
+## Exercício 3
 par(mfrow=c(1,2), pty="s") 
 par(pty="s")
 
@@ -15,3 +15,18 @@ plot(x, F_x, xlab="x", ylab = "F(x)", pch=16)
 segments(x, F_x, x+1, F_x)
 points (1:4, F_x[1:4])
 
+
+
+## Exercício 4
+x <- seq(1, 3, 0.1)
+theta <- 1
+k <- 1
+k2 <- 4
+
+f <- function(x, k) { (k*theta^k) / x^(k+1) }
+
+plot(x, f(x, k), type = "l", xlab = "x", ylab = "f(X)", xlim = c(1, 3), ylim = c(0, 4))
+lines(x, f(x, k2), type = "l", xlab = "x", ylab = "f(X)", xlim = c(1, 3), ylim = c(0, 4))
+abline(v = 1, lty = 3,  col = "red")
+
+     
