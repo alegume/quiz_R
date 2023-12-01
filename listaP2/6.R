a7c18030f52c37f46121ecfd0d9a214538a8272b
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2023-12-01 19:38:49 -0300

    ex6 P2

--- listaP2/6.R
+++ listaP2/6.R
@@ -6,20 +6,19 @@ s1 = .76
 s2 = .82
 
 gl = n1 + n2 - 2
+gl
 alpha = 0.1
 
 t = qt(alpha/2, df = gl, lower.tail = F)
 t
 
-
-n1 = n2 = 4
-s1 = 143.66
-s2 = 73.63
-
 s2_p = ( (n1 - 1)*s1 + (n2 - 1)*s2 ) / (n1 + n2 - 2) 
-
 s2_p
 
-round(,2)
-round(,2)
-round(,2)
+LI = (x1 - x2) - t * sqrt(s2_p * (1/n1 + 1/n2))
+LS = (x1 - x2) + t * sqrt(s2_p * (1/n1 + 1/n2))
+
+cat(LI, LS)
+
+round((x1 - x2) - 1.66 * sqrt(.79 * (1/n1 + 1/n2)), 2)
+round((x1 - x2) + 1.66 * sqrt(.79 * (1/n1 + 1/n2)), 2)
